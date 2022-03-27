0eceb820e298a6d7e27b7d68f98d573b0a25f665
Jin-uu <jinu200131@gmail.com>
2022-03-27 23:39:04 +0900

    ...copy.c mid commit

--- hw01/copy.c
+++ hw01/copy.c
@@ -4,12 +4,25 @@
 
 
 int main(int argc, char *argv[]){
-	FILE* file;
+	FILE* copy_file;
+	FILE* original_file;
 
-	file = fopen(argv[1],"w+");
-	if(file == NULL){
+	original_file = fopen(argv[1], "r");
+	if(original_file == NULL){
+		printf("파일을 여는데 실패했습니다. 종료합니다.\n");
+		return 0;
+	}
+
+	copy_file = fopen(argv[0],"w+");
+	if(copy_file == NULL){
 		printf("파일을 생성하는데 실패했습니다. 종료합니다.\n");
+		fclose(original_file);
 		return 0;		
 	}
+
+
+
+
+
 
 }
