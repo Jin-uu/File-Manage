2708c1dc0c1beeef27d977eea853766c9f09ffbf
Jin-uu <jinu200131@gmail.com>
2022-03-28 15:02:36 +0900

    *testfile 수정

--- hw01/test.c
+++ hw01/test.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void){
-	printf("hello world!\n");
+	char* one = "10";
+	char two = '2';
+	char three = '3';
+	// int first = atoi(one);
+	int sec = (int)one;
+	int third = (int)one;
+
+	char* str[3] = {"123", "234", "345"};
+	int first = atoi(str[1]);
+
+
+	printf("%d\n", first);
 	return 0;
 }
