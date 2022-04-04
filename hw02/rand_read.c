82eabe9cdbbe0946f0297bfb0df5940d6afdc70a
Jin-uu <jinu200131@gmail.com>
2022-04-04 16:26:27 +0900

    rand_read.c 시간측정제외 구현완료

--- hw02/rand_read.c
+++ hw02/rand_read.c
@@ -1,3 +1,5 @@
+#define DEBUG
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <time.h>
@@ -14,19 +16,55 @@ void swap(int *a, int *b);
 //
 // input parameters: 레코드 파일
 //
-int main((int argc, char **argv)
-{
+int main(int argc, char **argv){
 	int *read_order_list;
 	int num_of_records;
+	char *file_name;
+	FILE *target_file;
+
+	#ifdef DEBUG
+	file_name = "test_case_00.txt";
+	#endif
+	#ifndef DEBUG
+	file_name = argv[1];
+	#endif
+
+	target_file = fopen(file_name,"r");
+	if(target_file == NULL){
+		printf("error!\n");
+		return 0;
+	}
+
+	int file_start, file_end, file_size;
+	fseek(target_file,0,SEEK_SET);
+	file_start = ftell(target_file);
+	fseek(target_file,0,SEEK_END);
+	file_end = ftell(target_file);
+	file_size = file_end - file_start;
+
+	read_order_list = malloc(sizeof(int)*file_size);
+	num_of_records = file_size;
 
 	// 이 함수를 실행하면 'read_order_list' 배열에 읽어야 할 레코드 번호들이 순서대로
 	// 나열되어 저장됨. 'num_of_records'는 레코드 파일에 저장되어 있는 전체 레코드의 수를 의미함.
 	GenRecordSequence(read_order_list, num_of_records);
 
-
 	// 'read_order_list'를 이용하여 레코드 파일로부터 전체 레코드를 random 하게 읽어들이고,
 	// 이때 걸리는 시간을 측정하는 코드 구현
-
+	
+	int cnt=0;
+	for (int i = 0; i < file_size; i++){
+		fseek(target_file,read_order_list[i],SEEK_SET);
+		char cur_c = getc(target_file);
+		#ifdef DEBUG
+		cnt++;
+		printf("%c ",cur_c);
+		#endif
+	}
+	#ifdef DEBUG
+	printf("\n%d \n",cnt);
+	#endif
+	
 
 	return 0;
 }
