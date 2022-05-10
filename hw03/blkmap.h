771c6f56a4b108b6a75cc9035ca1361f7917380a
Jin-uu <jinu200131@gmail.com>
2022-05-10 19:55:48 +0900

    hw03 complete

--- hw03/blkmap.h
+++ hw03/blkmap.h
@@ -6,14 +6,14 @@
 
 #define	SECTOR_SIZE		512			
 #define	SPARE_SIZE		16			
-#define	PAGE_SIZE		(SECTOR_SIZE+SPARE_SIZE)
-#define 	SECTORS_PER_PAGE	1
-#define	PAGES_PER_BLOCK		32					// 수정 가능
-#define	SECTORS_PER_BLOCK	(SECTORS_PER_PAGE*PAGES_PER_BLOCK)
-#define	BLOCK_SIZE		(PAGE_SIZE*PAGES_PER_BLOCK)
-#define	BLOCKS_PER_DEVICE	1024					// 수정 가능
-#define	DEVICE_SIZE		(BLOCK_SIZE*BLOCKS_PER_DEVICE)
-#define	DATABLKS_PER_DEVICE	(BLOCKS_PER_DEVICE - 1)			// spare block로서 블록 하나를 사용
+#define	PAGE_SIZE		(SECTOR_SIZE+SPARE_SIZE)				// 528
+#define SECTORS_PER_PAGE	1									
+#define	PAGES_PER_BLOCK		4									// 수정 가능
+#define	SECTORS_PER_BLOCK	(SECTORS_PER_PAGE*PAGES_PER_BLOCK)	
+#define	BLOCK_SIZE		(PAGE_SIZE*PAGES_PER_BLOCK)				
+#define	BLOCKS_PER_DEVICE	8									// 수정 가능
+#define	DEVICE_SIZE		(BLOCK_SIZE*BLOCKS_PER_DEVICE)			
+#define	DATABLKS_PER_DEVICE	(BLOCKS_PER_DEVICE - 1)				// 1023 spare block로서 블록 하나를 사용
 //
 // 필요하면 상수를 추가해서 사용 가능함
 // 
